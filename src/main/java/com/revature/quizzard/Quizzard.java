a83e7b0861f8bc3df6a5d8540f00bd6cfe22adc0
Richard <rtcloud2@gmail.com>
2021-06-17 19:07:49 -0400

    starting off

---
+++ src/main/java/com/revature/quizzard/Quizzard.java
@@ -0,0 +1,20 @@
+package com.revature.quizzard;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quizzard implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quizzard.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+    }
+
+}
