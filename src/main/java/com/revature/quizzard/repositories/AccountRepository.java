638a206c43bf7cf79ed9499f79c9858649b9cb58
Richard <rtcloud2@gmail.com>
2021-06-18 15:09:30 -0400

    pushing changes for yml

--- src/main/java/com/revature/quizzard/repositories/AccountRepository.java
+++ src/main/java/com/revature/quizzard/repositories/AccountRepository.java
@@ -4,6 +4,7 @@ import com.revature.quizzard.models.user.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Integer> {
 
     AccountEntity findByUsername(String username);
