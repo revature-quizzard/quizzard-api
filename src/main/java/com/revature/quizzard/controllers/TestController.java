f49adb84dd407183bcc813a9b9ba9e4cd8159e9d
SeanTaba <shahramtaba@gmail.com>
2021-06-18 13:07:18 -0600

    Changed the testcontroller path to root for EB health

--- src/main/java/com/revature/quizzard/controllers/TestController.java
+++ src/main/java/com/revature/quizzard/controllers/TestController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/")
 public class TestController {
 
     @GetMapping
