23b706cf9423110c701cc61cbca9501e8e9af132
Nicholas Recino <nick.recino@icloud.com>
2021-06-18 18:44:30 -0400

    Merge conflicts that somehow snuck in

--- src/main/java/com/revature/quizzard/configs/AppConfig.java
+++ src/main/java/com/revature/quizzard/configs/AppConfig.java
@@ -3,7 +3,6 @@ package com.revature.quizzard.configs;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
