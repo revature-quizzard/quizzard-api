985a9309209a140ae2ed1caabee71f70000f629d
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-06-21 14:45:03 -0700

    Created subjects endpoint that returns all the subjects in the data base

---
+++ src/main/java/com/revature/quizzard/web/controllers/SubjectController.java
@@ -0,0 +1,31 @@
+package com.revature.quizzard.web.controllers;
+
+import com.revature.quizzard.models.flashcards.SubjectEntity;
+import com.revature.quizzard.services.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/subject")
+public class SubjectController {
+
+    private SubjectService subjectService;
+
+    @Autowired
+    public SubjectController(SubjectService subjectService){
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping(value = "/all", produces = APPLICATION_JSON_VALUE)
+    public List<SubjectEntity> getSubjects(HttpServletRequest req){
+        return subjectService.getAllSubjects();
+    }
+
+}
