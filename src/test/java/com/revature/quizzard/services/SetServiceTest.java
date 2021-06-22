f757e8b9940c8f43dcd6fbba670730d26a3c5d90
Vinson <55514029+vinsonchin@users.noreply.github.com>
2021-06-22 15:16:45 -0600

    finished the unit tests for created sets

--- src/test/java/com/revature/quizzard/services/SetServiceTest.java
+++ src/test/java/com/revature/quizzard/services/SetServiceTest.java
@@ -2,11 +2,16 @@ package com.revature.quizzard.services;
 
 import com.revature.quizzard.dtos.*;
 import com.revature.quizzard.exceptions.ResourceNotFoundException;
+import com.revature.quizzard.models.flashcards.CardEntity;
+import com.revature.quizzard.models.sets.SetEntity;
 import com.revature.quizzard.models.user.AccountEntity;
 import com.revature.quizzard.repositories.*;
 import org.junit.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
@@ -16,8 +21,10 @@ public class SetServiceTest {
     private SetService sut;
     private SetRepository mockSetRepo;
     private AccountRepository mockAccountRepo;
-    private SetDTO setDTO;
+    private SetEntity mockSetEntity;
+    private List<SetEntity> mockSetList;
     private AccountEntity mockAccount;
+    private Set<CardEntity> mockCards;
 
     @Before
     public void setupTest() {
@@ -31,23 +38,35 @@ public class SetServiceTest {
         mockSetRepo = null;
         mockAccountRepo = null;
         sut = null;
-        setDTO = null;
+        mockSetEntity = null;
         mockAccount = null;
+        mockSetList = null;
+        mockCards = null;
     }
 
     @Test
     public void test_getCreatedSetsWithValidUsernameAndSet() {
-        mockAccount.setUsername("test");
-        setDTO = new SetDTO(1,"test", true, mockAccount);
+
+        mockAccount = new AccountEntity();
+        mockSetList = new ArrayList<>();
+        mockCards = new HashSet<>();
+        mockSetEntity = new SetEntity(1, mockCards, mockAccount, "test", true);
+        mockSetList.add(mockSetEntity);
+        when(mockAccountRepo.findByUsername(any())).thenReturn(mockAccount);
+        when(mockSetRepo.findAllCreatedByAccount(any())).thenReturn(mockSetList);
 
         List<SetDTO> result = sut.getCreatedSets("test");
 
-        assertEquals(result.size(), 1);
+        assertEquals(1, result.size());
     }
 
     @Test
     public void test_getCreatedSetsWithValidUsernameAndNoSet() {
-        mockAccount.setUsername("test");
+
+        mockAccount = new AccountEntity();
+        mockSetList = new ArrayList<>();
+        when(mockAccountRepo.findByUsername(any())).thenReturn(mockAccount);
+        when(mockSetRepo.findAllCreatedByAccount(any())).thenReturn(mockSetList);
 
         List<SetDTO> result = sut.getCreatedSets("test");
 
